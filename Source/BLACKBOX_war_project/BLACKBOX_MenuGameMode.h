66911f7c40251639febb4743df3013c605a97af2
Julien Saevecke <Julien.Saevecke@udo.edu>
2018-06-03 00:13:52 +0200

    Add infinity warriors pack Add menu level Add Huds/Widgets for test menus

---
+++ Source/BLACKBOX_war_project/BLACKBOX_MenuGameMode.h
@@ -0,0 +1,20 @@
+// Copyright 2018, Julien Saevecke, All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "BLACKBOX_MenuGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLACKBOX_WAR_PROJECT_API ABLACKBOX_MenuGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
